82ff4f57c597c56563230f423690b356f351dd33
Gaizka Allende <gaizka@allende.org>
2021-01-15 17:47:10 +0000

    Delete difference directory

--- difference/difference.js
+++
@@ -1,9 +0,0 @@
-/*
-Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
-*/
-export default function difference(inspect, exclude) {
-  if (!Array.isArray(inspect) || !Array.isArray(exclude)) {
-    return inspect;
-  }
-  return inspect.filter((item) => !exclude.includes(item));
-}
