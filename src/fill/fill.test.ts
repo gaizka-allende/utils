9a34bd30bb6b693cdbd312715e8d999913bfb1d7
Gaizka Allende <gaizka@allende.org>
2021-01-29 14:22:23 +0000

    add support for start only argument in fill

--- src/fill/fill.test.ts
+++ src/fill/fill.test.ts
@@ -4,7 +4,10 @@ describe("fill", () => {
   test("fills all elements", () => {
     expect(fill([1, 2, 3], "a")).toEqual(["a", "a", "a"]);
   });
-  test("partly fills the array", () => {
+  test("partly fills the array using start and end", () => {
     expect(fill([4, 6, 8, 10], "*", 1, 3)).toEqual([4, "*", "*", 10]);
+  });
+  test("partly fills the array using only start", () => {
+    expect(fill([4, 6, 8, 10], "*", 1)).toEqual([4, "*", "*", "*"]);
   });
 });
--- src/fill/fill.ts
+++ src/fill/fill.ts
@@ -1,10 +1,11 @@
 /* Fills elements of array with value from start up to, but not including, end. */
-export default function fill(array: any[], value: any, start: number, end: number) {
+export default function fill(array: any[], value: any, start?: number, end?: number): any[] {
   if (start === undefined) {
     return array.map((x) => value);
   }
+  const endIndex = end || array.length;
   return array.map((x, index) => {
-    if (index >= start && index < end) {
+    if (index >= start && index < endIndex) {
       return value;
     }
     return x;
